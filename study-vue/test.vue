8a8fd241d0287d0e977e6c261f386205746ea20a
xul <2736219389@qq.com>
2018-08-08 15:41:24 +0800

    111

---
+++ study-vue/test.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mixins: [require('')],
+        data() {
+            return {
+                name: 'name'
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        }
+    }
+    var vm = new Vue({
+        data() {
+            return {
+                key: value
+            }
+        },
+        computed: {
+            name() {
+                return this.data            
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        watch: {
+            data(newValue, oldValue) {
+                
+            }
+        },
+        watch: {
+            question:function(newValue, oldValue) {
+                
+            }
+        },
+    })
+    vm.$watch(expOrFn, callback)
+</script>
+
+<style scoped>
+
+</style>
+
